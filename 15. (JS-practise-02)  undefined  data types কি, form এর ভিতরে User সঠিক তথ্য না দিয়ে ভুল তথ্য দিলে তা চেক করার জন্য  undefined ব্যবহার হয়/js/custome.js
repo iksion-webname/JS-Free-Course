0efd778ef34d45caf1d361c8a18f360daa73c5cc
iksion-webname <selimgithub25@gmail.com>
2025-11-14 12:30:54 +0600

    Add files via upload

---
+++ 15. (JS-practise-02)  undefined  data types কি, form এর ভিতরে User সঠিক তথ্য না দিয়ে ভুল তথ্য দিলে তা চেক করার জন্য  undefined ব্যবহার হয়/js/custome.js
@@ -0,0 +1,9 @@
+"use strict";
+alert("Allaha");
+
+/* এখানে variable তৈরি করা হয়েছে কিন্তু কোন Value দেওয়া হয় নি, তাই alert(fastName); করলে Undefined দেখাবে*/
+
+let fastName;
+alert(fastName);
+
+/* যদি "name" variable টা Unedfined না হয় এবং যদি variable টা Null না হয় এবং যদি variable টা  কোর্ড কোর্ড  ( ফাকা ) না হয় তাহলে বুঝতে হবে User input Box এর ভিতরে Value দিয়েছে । */
