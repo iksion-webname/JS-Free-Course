a93a47073a2af1acca8413d76dee3b989491a1c1
iksion-webname <selimgithub25@gmail.com>
2025-11-14 15:46:01 +0600

    Add files via upload

---
+++ 17. JS- Prompt ( প্রম্পট  তথ্য গ্রহন কারী box) এবং  Confirm Box ( কনফার্ম বক্স) নিয়ে বিস্তারিত আলোচনা/js/coustom.js
@@ -0,0 +1,14 @@
+"ues strict";
+alert("Anue");
+
+let number = prompt(420);
+console.log(number);
+
+let fName = prompt("Enter your Name :"); // কেবল prompt এর মাধ্যমে input Box তৈরি হবে
+console.log(fName);
+
+let mname = prompt("Enter your name :", "Shain");
+//console.log(mname);
+console.log(typeof mname);
+
+confirm("Are you Rahim ?");
