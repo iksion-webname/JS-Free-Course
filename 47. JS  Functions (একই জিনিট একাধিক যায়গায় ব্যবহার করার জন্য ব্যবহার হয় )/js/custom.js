bf4d85d966271d187a8c7622118fd74d14e37318
iksion-webname <selimgithub25@gmail.com>
2025-12-03 00:29:48 +0600

    Add files via upload

---
+++ 47. JS  Functions (একই জিনিট একাধিক যায়গায় ব্যবহার করার জন্য ব্যবহার হয় )/js/custom.js
@@ -0,0 +1,35 @@
+"use strict";
+
+alert("Zhumu");
+
+function myTutoril() {
+    console.log("Selim Reza");
+    console.log("html");
+    console.log("CSS");
+    console.log("Javascript");
+    console.log("------------");
+}
+
+myTutoril();
+
+console.log("=================");
+console.log("First");
+console.log("=================");
+
+myTutoril(); // (18 মিনিট ) কেবলমাত্র function এর নামকে callin বলে । এখানে callin হলো myTutoril();
+
+myTutoril();
+
+myTutoril();
+
+/* ব্যাখ্যা :  এখানে function  এর নাম দেওয়া হয়েছে myTutoril()এই function এর ভিতরে যতগুলো সূত্র লেখা হয়েছে সবগুলো javascript page এর ভিতরে যত যায়গাল  লেখার প্রয়োজন হবে সব যায়গাতে কেবলমাত্র  myTutoril();বসিয়ে দিলেই হয়ে যাবে ।  */
+
+//==========================================
+console.log("Selim Reza");
+console.log("html");
+console.log("CSS");
+console.log("Javascript");
+console.log("------------");
+//==========================================
+
+function name(params) {}
